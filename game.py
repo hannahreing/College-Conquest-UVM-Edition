7ed3c08dc889bfb1c5af1adaba4efd0b5e8e537f
Hannah Reing <hsreing12@gmail.com>
2025-11-28 18:08:52 -0500

    Playing around with tk

--- game.py
+++ game.py
@@ -6,6 +6,19 @@ CS 1210 G
 
 import random
 import math
-import turtle
 import tkinter as tk
 
+window = tk.Tk()
+window.title("College Conquest UVM Edition")
+canvas = tk.Canvas(window, width=400, height=400, bg="lightgreen")
+canvas.grid()
+
+for x in range(11):
+    for y in range(11):
+        text = f"({x}, {y})"
+        label = tk.Label(text=text, foreground="darkblue")
+        label.grid(column=y, row=x)
+
+# greeting = tk.Label(text="Hello World")
+# greeting.pack()
+window.mainloop()
